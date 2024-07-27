2e3c73a68d0fc8b1d175ed7728827f1b83a494da
Jhon-Crow <jhamish@mail.ru>
2024-07-27 19:54:03 +0300

    add mapping

--- src/services/stateService.js
+++ src/services/stateService.js
@@ -1,23 +1,51 @@
 import {saveToLocalStorage} from "../helpers/saveToLocalStorage.js";
 import {getFromLocalStorage} from "../helpers/getFromLocalStorage.js";
 
-const startArray = getFromLocalStorage()
+const startArray = getFromLocalStorage() || []
 
-console.log('START',startArray)
-
-
+console.log('START', startArray)
 
 
 
 
+const stateMap = new Map();
+startArray.forEach(obj => {
+    stateMap.set(obj.id, obj)
+})
+
+console.log(stateMap)
+
+export function stateService(action, toDoItemId){
+    switch (action){
+        case 'SET':
+            break;
+        case 'PATCH':
+            break;
+        case 'GET':
+            // stateMap.get(toDoItemId)
+            break;
+        case 'DELETE':
+            break;
+    }
+}
+
+console.log(startArray)
+
+
+// let endArray = [...startArray, {
+//     id: Date.now(),
+//     deadline: 270724,
+//     text: 'string',
+//     isDone: undefined
+// }]
 
 
 
-
-
-
-
+// console.log(endArray)
+//
 window.addEventListener('unload', () => {
+    const endArray = Array.from(stateMap.values());
+
     if (startArray !== endArray){
         saveToLocalStorage(endArray)
     }
