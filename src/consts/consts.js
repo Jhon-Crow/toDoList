0ce4cd25cf2803d66cdec22787dbbf170d326cbe
Jhon-Crow <jhamish@mail.ru>
2024-07-28 10:41:32 +0300

    add LOCALSTORAGE_TODO_DATA key in consts

---
+++ src/consts/consts.js
@@ -0,0 +1,1 @@
+export const LOCALSTORAGE_TODO_DATA = 'toDoListData'
--- src/helpers/saveToLocalStorage.js
+++ src/helpers/saveToLocalStorage.js
@@ -1,3 +1,5 @@
+import {LOCALSTORAGE_TODO_DATA} from "../consts/consts.js";
+
 export function saveToLocalStorage(arrayOfObjects){
-    localStorage.setItem('toDoListData', JSON.stringify(arrayOfObjects))
+    localStorage.setItem(LOCALSTORAGE_TODO_DATA, JSON.stringify(arrayOfObjects))
 }
