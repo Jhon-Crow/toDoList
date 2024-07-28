4ed47eea42e276178adfe70caaff578fda86f7b1
Jhon-Crow <jhamish@mail.ru>
2024-07-28 08:07:36 +0300

    add btn & creating logic in toDoForm.js

--- src/components/toDoForm.js
+++ src/components/toDoForm.js
@@ -1,9 +1,9 @@
 class ToDoItem {
     constructor(id, deadline, text, isDone) {
-            this.id = String(id)
-            this.deadline = Number(deadline)
-            this.text = String(text)
-            this.isDone = isDone
+        this.id = String(id);
+        this.deadline = Number(deadline);
+        this.text = String(text);
+        this.isDone = isDone;
     }
 }
 
@@ -12,21 +12,32 @@ class ToDoInput {
         this.inputElement = document.createElement('input');
         this.inputElement.id = String(id);
         this.inputElement.type = type;
-        this.inputElement.placeholder = placeholder;
+        this.inputElement.placeholder = placeholder || '';
     }
     getElement() {
         return this.inputElement;
     }
 }
 
-const toDoTextInput = new ToDoInput('toDoInput', 'text', 'task text');
-const toDoDataInput = new ToDoInput('toDoInput', 'datetime-local');
+const toDoTextInput = new ToDoInput('toDoInputText', 'text', 'task text');
+const toDoDataInput = new ToDoInput('toDoInputData', 'datetime-local');
 
 document.body.appendChild(toDoTextInput.getElement());
 document.body.appendChild(toDoDataInput.getElement());
 
+const button = document.createElement('button');
+button.textContent = 'Add ToDo';
+document.body.appendChild(button);
 
+button.addEventListener('click', () => {
+    const text = toDoTextInput.getElement().value;
+    const deadline = new Date(toDoDataInput.getElement().value).getTime();
+    if (text && deadline) {
+        const newToDo = new ToDoItem(Date.now(), deadline, text, undefined);
 
-
-
-// stateService.setToService(test.id, test)
+        console.log(newToDo);
+        stateService.setToService(newToDo.id, newToDo);
+    } else {
+        alert('empty input')
+    }
+});
