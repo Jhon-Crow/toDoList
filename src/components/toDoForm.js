8566f6fcdcbf9fa873d6a8d7d0b51e87bb12219c
Jhon-Crow <jhamish@mail.ru>
2024-07-28 07:29:50 +0300

    add simple input class & add 2 of this in body
    add style css

--- src/components/toDoForm.js
+++ src/components/toDoForm.js
@@ -7,8 +7,26 @@ class ToDoItem {
     }
 }
 
+class ToDoInput {
+    constructor(id, type, placeholder) {
+        this.inputElement = document.createElement('input');
+        this.inputElement.id = String(id);
+        this.inputElement.type = type;
+        this.inputElement.placeholder = placeholder;
+    }
+    getElement() {
+        return this.inputElement;
+    }
+}
+
+const toDoTextInput = new ToDoInput('toDoInput', 'text', 'task text');
+const toDoDataInput = new ToDoInput('toDoInput', 'datetime-local');
+
+document.body.appendChild(toDoTextInput.getElement());
+document.body.appendChild(toDoDataInput.getElement());
 
 
 
 
 
+// stateService.setToService(test.id, test)
