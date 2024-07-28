a234ead6a5a384fdfab3a22ed5ebf543e1da3e51
Jhon-Crow <jhamish@mail.ru>
2024-07-28 06:32:28 +0300

    add class ToDoItem in toDoForm.js

---
+++ src/components/toDoForm.js
@@ -0,0 +1,14 @@
+class ToDoItem {
+    constructor(id, deadline, text, isDone) {
+            this.id = String(id)
+            this.deadline = Number(deadline)
+            this.text = String(text)
+            this.isDone = isDone
+    }
+}
+
+
+
+
+
+
